45d3b4df7799832b89605e7274118f5e91b701ec
BlackOmegaSF <kian.leiner@gmail.com>
2022-01-02 10:09:50 -0800

    Update death messages to use Component

--- src/main/java/com/floogoobooq/blackomega/paperdeathmessages/PaperDeathMessages.java
+++ src/main/java/com/floogoobooq/blackomega/paperdeathmessages/PaperDeathMessages.java
@@ -1,13 +1,18 @@
 package com.floogoobooq.blackomega.paperdeathmessages;
 
+import java.util.Objects;
 import java.util.Random;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class PaperDeathMessages extends JavaPlugin implements Listener {
@@ -31,6 +36,7 @@ public class PaperDeathMessages extends JavaPlugin implements Listener {
             return;
         }
         DamageCause cause = player.getLastDamageCause().getCause();
+        String displayName = serializeComponent(player.displayName());
 
         StringBuilder stringBuilder = new StringBuilder();
 
@@ -38,28 +44,28 @@ public class PaperDeathMessages extends JavaPlugin implements Listener {
         if(cause == DamageCause.DROWNING) { //Death by drowning
             int drownRand = random.nextInt(2);
             if(drownRand == 0) {
-                stringBuilder.append(player.getDisplayName());
+                stringBuilder.append(displayName);
                 stringBuilder.append(" forgot how to swim");
             } else if(drownRand == 1) {
-                stringBuilder.append(player.getDisplayName());
+                stringBuilder.append(displayName);
                 stringBuilder.append(" discovered they don't have gills");
             }
 
         } else if(cause == DamageCause.LIGHTNING) { //Death by lightning (not fire afterwards)
-            stringBuilder.append(player.getDisplayName());
+            stringBuilder.append(displayName);
             stringBuilder.append(" built up too much charge");
 
         } else if(cause == DamageCause.FLY_INTO_WALL) { //Death by hitting a wall with elytra (not when hitting ground)
             int flyRand = random.nextInt(3);
             if(flyRand == 0) {
-                stringBuilder.append(player.getDisplayName());
+                stringBuilder.append(displayName);
                 stringBuilder.append(" forgot their flying lessons");
             } else if(flyRand == 1) {
                 stringBuilder.append("Oof ouch owie ");
-                stringBuilder.append(player.getDisplayName());
+                stringBuilder.append(displayName);
                 stringBuilder.append("'s bones");
             } else if(flyRand == 2) {
-                stringBuilder.append(player.getDisplayName());
+                stringBuilder.append(displayName);
                 stringBuilder.append(" performed a controlled flight into terrain");
             }
 
@@ -67,21 +73,22 @@ public class PaperDeathMessages extends JavaPlugin implements Listener {
             if(player.getKiller() != null) {
                 Player killer = player.getKiller();
                 String bitchwhipper = "The Bitchwhipper";
-                String name = killer.getInventory().getItemInMainHand().getItemMeta().getDisplayName();
+                ItemMeta itemMeta = killer.getInventory().getItemInMainHand().getItemMeta();
+                String name = itemMeta.displayName() == null ? "" : serializeComponent(itemMeta.displayName());
                 if (name.equals(bitchwhipper)) {
-                    stringBuilder.append(killer.getDisplayName());
+                    stringBuilder.append(PlainTextComponentSerializer.plainText().serialize(killer.displayName()));
                     stringBuilder.append(" bitchwhipped ");
-                    stringBuilder.append(player.getDisplayName());
+                    stringBuilder.append(displayName);
                     player.getWorld().playSound(player.getLocation(), "custom.whip", 1F, 1F);
                 } else {
-                    stringBuilder.append(event.getDeathMessage());
+                    stringBuilder.append(serializeComponent(event.deathMessage()));
                 }
             } else {
-                stringBuilder.append(event.getDeathMessage());
+                stringBuilder.append(serializeComponent(event.deathMessage()));
             }
 
         } else {
-            stringBuilder.append(event.getDeathMessage());
+            stringBuilder.append(serializeComponent(event.deathMessage()));
         }
 
         stringBuilder.append(" at [");
@@ -92,8 +99,17 @@ public class PaperDeathMessages extends JavaPlugin implements Listener {
         stringBuilder.append(player.getLocation().getBlockZ());
         stringBuilder.append("]");
 
-        event.setDeathMessage(stringBuilder.toString());
+        TextComponent deathMessage = Component.text(stringBuilder.toString());
+        event.deathMessage(deathMessage);
 
         player.getWorld().playSound(player.getLocation(), "custom.oof", 1F, 1F);
+    }
+
+    private String serializeComponent(Component component) {
+        if (component == null) {
+            return "";
+        } else {
+            return PlainTextComponentSerializer.plainText().serialize(component);
+        }
     }
 }
